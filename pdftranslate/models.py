00737cc5ffbb94c4fa728539003c50764c83c0ef
Saken Serik <sakenserik@Sakens-MacBook-Air.local>
2025-03-01 13:20:43 +0500

    added mobile adaptation to login html

--- pdftranslate/models.py
+++ pdftranslate/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from datetime import timedelta
-import mathgi
+import math
 
 
 class PDFDocument(models.Model):
